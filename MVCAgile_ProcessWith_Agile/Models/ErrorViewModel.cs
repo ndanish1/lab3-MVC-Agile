35c60785695660954cdc6c707b9a79de133911e5
Naveed Danish <ndanish1@my.centennialcollege.ca>
2024-03-18 21:41:02 -0400

    Add project files.

---
+++ MVCAgile_ProcessWith_Agile/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCAgile_ProcessWith_Agile.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

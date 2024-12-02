34caa72b6cb292a943b9fbad7bc6c83d96cc4fd2
Naveed Danish <ndanish1@my.centennialcollege.ca>
2024-12-01 22:02:04 -0500

    change

--- MVCAgile_ProcessWith_Agile/Models/SeedData.cs
+++ MVCAgile_ProcessWith_Agile/Models/SeedData.cs
@@ -19,7 +19,7 @@ public static class SeedData
             context.Movie.AddRange(
                 new Movie
                 {
-                    Title = "When Harry Met Sally",
+                    Title = "When Harry Met Sallys",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
                     Rating = "R",
